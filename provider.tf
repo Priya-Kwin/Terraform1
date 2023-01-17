a8f121c2af04a1affb082c03af7706c3f18f698b
Priya <priya@gmail.com>
2023-01-17 15:46:00 -0800

    task1

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = "035f1e5a-b0a1-4f50-9fd6-67fd9f77af81"
+  client_id       = "25dde1f7-edff-4e2f-af5a-965a46b45958"
+  client_secret   = "WgQ8Q~Ld48OO1dR741qW7QEV6gMyaQJP9nfbHded"
+  tenant_id       = "0dd1b131-a558-485f-9a71-814bd729622f"
+}
